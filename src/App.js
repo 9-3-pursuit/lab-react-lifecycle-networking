d979ee03ef165dde2644420e429e62bed35d7dad
joshuanelsondev <joshuanelsondev@gmail.com>
2023-02-24 14:36:38 -0500

    Create fetch for retrieving employee data and render to the page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+
+import React, { useState } from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
 
-function App () {
+function App() {
+  const [day, setDay] = useState("02/24/23");
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <NavBar />
       <EmployeeList />
+      <button
+        onClick={() => setDay(day === "02/24/23" ? "02/25/23" : "02/24/23")}
+      >
+        Change Day
+      </button>
+      <button onClick={() => setCount(count + 1)}>
+        Force App to re-render
+      </button>
     </>
   );
-};
+}
 
 export default App;
--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,14 +1,37 @@
+import { useState, useEffect } from "react";
 import PetList from "./PetList";
 import "./Employee.css";
 
 export const Employee = () => {
+  const API_BASE = "https://vet-app-0obi.onrender.com/";
+  const [employeeData, setEmployeeData] = useState([])
+
+  useEffect(() => {
+    const url = `${API_BASE}api/employees`;
+    function fetchEmployee() {
+      fetch((url))
+       .then((response) => response.json())
+       .then((data) => {
+        setEmployeeData(data);
+       })
+    }
+    fetchEmployee()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
+
   return (
-    <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
-    </article>
+    employeeData.map(employee => {
+      return(
+        <article className="employee">
+          <h3>{employee.prefix} {employee.firstName} {employee.lastName}{employee.postfix && `, ${employee.postfix}`}</h3>
+          <h4>{employee.title}</h4>
+          <button>Show Pets</button>
+          <PetList />
+        </article>
+      )
+    })
+    
   );
 };
 
