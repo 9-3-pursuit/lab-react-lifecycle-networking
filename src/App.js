47a1f609958ecf70761c88b17999ddd29e56823e
Keeanu Rodriguez <keeanu7@gmail.com>
2023-02-24 14:00:03 -0500

    fetch called and name formatted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
+import { useState, useEffect } from "react";
 
 function App () {
+  const [employees, setEmployees] = useState([])
+  //const [pets, setPets] = useState({ name: "", kind: "" })
+  
+  
+
+  function getEmployeesData () {
+    const employees = "https://vet-app-0obi.onrender.com/api/employees"
+    fetch(employees)
+    .then((results) => results.json())
+    .then((employeesData) => {
+      setEmployees(employeesData);
+      // console.log(employeesData)
+      // console.log(employees)
+    })
+    .catch((error) => console.log(error))
+  }
+
+  // function getPetsData () {
+  //   const pets = "https://vet-app-0obi.onrender.com/api/pets"
+  //   fetch(pets)
+  //   .then((results) => results.json())
+  //   .then((petsData) => {
+  //     console.log("we also here");
+  //     setPets(petsData);
+  //   })
+  //   .catch((error) => console.log(error))
+  // }
+
+  useEffect(() => {
+    //getEmployeesData();
+    //getPetsData();
+  }, [])
+
+
   return (
     <>
       <NavBar />
-      <EmployeeList />
+      <EmployeeList employees={employees} />
     </>
   );
 };
--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,12 +1,12 @@
 import PetList from "./PetList";
 import "./Employee.css";
 
-export const Employee = () => {
+export const Employee = ({employee}) => {
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
+      <h3><h2>{`${employee.prefix} ${employee.firstName} ${employee.lastName}${employee.postfix ? `, ${employee.postfix}` : ""}`}</h2></h3>
+      <h4>{employee.title}</h4>
+      <button >Show Pets</button>
       <PetList />
     </article>
   );
