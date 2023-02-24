9962ae7758f3dc9198438869f6dc23229e9f20f2
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-02-24 14:04:03 -0500

    started lab in class

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    fetch("https://vet-app-0obi.onrender.com/api")
+      .then((res) => res.json())
+      .then((apiData) => {
+        console.log(apiData);
+        setEmployees(apiData);
+      });
+  }, []);
+
   return (
     <main>
       <h2>All Staff</h2>
       <section className="employee-list">
-        <Employee />
+        {employees.map((employee, i) => (
+          <Employee key={employee.id} />
+        ))}
       </section>
     </main>
   );
