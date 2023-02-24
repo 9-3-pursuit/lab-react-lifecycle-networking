a35a3179971a1cc3fa6f6d97d5e952821dca9a1c
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-02-24 12:56:56 -0500

    Adds API call to for employee list

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -3,6 +3,18 @@ import Employee from "./Employee";
 import "./EmployeeList.css";
 
 export const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+
+    if (!ignore) {
+      fetchEmployeeData(setEmployees).catch(console.error);
+    }
+
+    return () => (ignore = true);
+  }, []);
+
   return (
     <main>
       <h2>All Staff</h2>
@@ -12,5 +24,14 @@ export const EmployeeList = () => {
     </main>
   );
 };
+
+async function fetchEmployeeData(setEmployees) {
+  const FETCH_URL = "https://vet-app-0obi.onrender.com";
+  const EMPLOYEE_ENDPOINT = "/api/employees";
+  const employeeDataRaw = await fetch(FETCH_URL + EMPLOYEE_ENDPOINT);
+  const employeeData = await employeeDataRaw.json();
+
+  setEmployees(employeeData);
+}
 
 export default EmployeeList;
