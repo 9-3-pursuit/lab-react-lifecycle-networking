06687307302fca791de451245b0b06b9d42f3b14
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-26 18:30:46 -0500

    fetch api

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import Employee from "./Employee";
 import "./EmployeeList.css";
 
+const API_BASE =  ""
 export const EmployeeList = () => {
+
+  const [employee, setEmployee] = useState([])
+
+  useEffect(() => {
+    function getEmployeeInfo(){
+      const url = `${API_BASE}`
+    }
+  })
+
   return (
     <main>
       <h2>All Staff</h2>
