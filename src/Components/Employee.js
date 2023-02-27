f34e9ad89849e205cb2b4d4474cbb425794f262f
touhami-ben <brooklyn.ben76@gmail.com>
2023-02-27 00:25:13 -0500

    lifecycle-networking

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,13 +1,39 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState} from"react";
 
-export const Employee = () => {
+const URL = `https://vet-app-0obi.onrender.com/api/pets`
+export const Employee = ({employee}) => {
+  const[clicked, setClicked] = useState(false);
+  const [petArr, setPetArr] = useState([]);
+
+  function handleClick(){
+    setClicked(!clicked);
+    getPets();
+
+  }
+
+  function getPets(){
+    fetch(`${URL}?employeeId=${employee.id}`)
+    .then((response) => response.json())
+    .then((pets) => {
+      setPetArr([...pets]);
+    })
+  }
+  
+
+  
+  
+
+  
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{`${employee.prefix} ${employee.firsName} ${employee.lastName} ${employee.postfix ? ", " + employee.postfix : ""}`}</h3>
+      <h4>{employee.title}</h4>
+      <button onClick = {handleClick}>Show Pets</button>
+
+      {clicked?
+      <PetList petArr={petArr}/> : null }
     </article>
   );
 };
