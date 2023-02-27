3c4e32a148914f92a021cfac3d15b50590dd68b0
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-02-26 21:40:28 -0500

    code for employee, employeeList, PetList, Format

---
+++ src/Components/utils/format.js
@@ -0,0 +1,5 @@
+// input: employee object
+// output: formatted name as a string
+export const formatEmployee = (firstName, lastName, prefix, postfix, title ) => {
+    return `${prefix} ${firstName} ${lastName}, ${postfix} `
+};
