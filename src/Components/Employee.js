2e29eae6ee222f7d9c63bd1340a976ee05147437
Diandre Miller <millerdiandre@gmail.com>
2023-02-28 15:40:21 -0500

    correct api used

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -2,23 +2,13 @@ import PetList from "./PetList";
 import "./Employee.css";
 import { useState, useEffect } from "react";
 
-const BASE_URL = 'https://vet-app-0obi.onrender.com/api/employees/'
+
+// const BASE_URL = 'https://vet-app-0obi.onrender.com/api'
 
 export const Employee = ({ firstName, lastName, prefix, postfix, title, id }) => {
   
   const [showPets, setShowPets] = useState(false);
-  const [pets, setPets] = useState([]);
-
-  useEffect(() => {
-    fetch(`${BASE_URL}${id}/pets`)
-      .then((response) => response.json())
-      .then((data) => {
-        setPets(data);
-        console.log(data)
-
-      });
-  }, [id]);
-
+ 
   const togglePets = () => {
     setShowPets(!showPets);
   }
@@ -28,7 +18,7 @@ export const Employee = ({ firstName, lastName, prefix, postfix, title, id }) =>
       <h3>{prefix} {firstName} {lastName}, {postfix} </h3>
       <h4>{title}</h4>
       <button onClick={togglePets}>{showPets ? 'Hide Pets' : 'Show Pets'}</button> 
-      {showPets && <PetList pets={pets} />}
+      {showPets && <PetList id={id} />}
     </article>
   );
 };
--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,11 +1,28 @@
-const PetList = ({ pets }) => {
+import { useState, useEffect } from "react";
+
+
+const BASE_URL = `https://vet-app-0obi.onrender.com/api/pets?employeeId=`
+const PetList = ({ id }) => { 
+
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setPets(data);
+        console.log(data)
+
+      });
+  }, [id]);
+
 
   return (
     <aside className="pets-list">
-      {pets && pets.length >0 ? 
+      { (pets.length >0 ? 
         <p>{pets.map(pet => pet.name).join(', ')}</p>
-        : <p>No pets are listed for this employee</p>
-      }
+        : <p>No pets listed for this employee</p>
+      )}
     </aside>
   );
 };
