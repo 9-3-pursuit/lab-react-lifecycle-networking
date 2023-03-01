4e8851ba63576335021c78a20a81487e23b5e4ea
Kerridene Small <Kerridenesmall@gmail.com>
2023-03-01 14:02:16 -0500

    Completed Lifecycle Lab

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,15 +1,19 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState } from "react";
 
-export const Employee = () => {
+export const Employee = ({ employee }) => {
+  const [showPets, setShowPets] = useState(false)
+  console.log(showPets)
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{employee.prefix} {employee.firstName} {employee.lastName}{employee.postfix && `, ${employee.postfix}`}</h3>
+      <h4>{employee.title}</h4>
+      <button onClick={() => { setShowPets(!showPets) }}>Show Pets</button>
+      <PetList employeeId={employee.id} />
     </article>
-  );
+  )
+
 };
 
 export default Employee;
