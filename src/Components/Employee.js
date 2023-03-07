6357b09057557ad09e8eeb129f573418ebd4fdac
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-03-07 18:16:51 -0500

    still not showing "no pets"

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -7,28 +7,18 @@ import { useState, useEffect} from "react";
 
 export const Employee = ({firstName, lastName, prefix, postfix, title, id}) => {
   const [showPets, setShowPets]= useState(false);
-  // const [pets, setPets] = useState([]);
-  // console.log(id)
-  // useEffect(() => {
-    
-  //   const url = `https://vet-app-0obi.onrender.com/api/pets`;
-  //   fetch(url)
-  //       .then((res) => res.json())
-  //       .then((data) => {
-  //           console.log(data)
-  //           setPets(data);
-  //       });
-  // }, [])
+ 
 
   return (
     <article className="employee">
       {/* <h3>formatEmployee</h3> */}
-      <h3>{prefix ? prefix: null} {firstName} {lastName} {postfix ? postfix: null}</h3>
+      <h3>{prefix ? prefix: null} {firstName} {lastName}, {postfix ? postfix: null}</h3>
+      <br></br>
       <h4>{title}</h4>
       <button onClick={() => {setShowPets(!showPets)}}>
-        {showPets ? "Hide Pets": "Show Pets"}
+        {showPets ? "Hide pets": "Show Pets"}
         </button>
-     {showPets && <PetList id={id}/>}
+     {showPets && <PetList id={id}/>} 
     </article>
   );
 };
